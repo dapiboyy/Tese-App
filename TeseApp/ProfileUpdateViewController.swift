06c748b36afee7603ec9e7e4a68302b3a0ee7f5f
Tatenda Dapi <tatenda@Tatendas-MacBook-Air.local>
2017-11-15 18:47:18 +0200

    Initial Commit

---
+++ TeseApp/ProfileUpdateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileUpdateViewController.swift
+//  TeseApp
+//
+//  Created by Tatenda Dapi on 10/11/2017.
+//  Copyright © 2017 Tatenda Dapi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileUpdateViewController: UIViewController {
+
+    @IBOutlet weak var uniListText: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
